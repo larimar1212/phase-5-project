32e968f73d54ce50be48354f83d57515262472ac
larimar fondeur <larimarfondeur@gmail.com>
2022-10-11 18:18:47 -0400

    2nd seeds

--- db/migrate/20221011215150_create_prompts.rb
+++ db/migrate/20221011215150_create_prompts.rb
@@ -1,7 +1,7 @@
 class CreatePrompts < ActiveRecord::Migration[7.0]
   def change
     create_table :prompts do |t|
-      t.string :content
+      t.text :content
       t.integer :user_id 
 
       t.timestamps
